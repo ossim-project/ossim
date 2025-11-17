db2fe217cb5eaa0bfc5f314a40c5356ceddce48b
Yiliang Wan <yiliangw@hotmail.com>
2025-11-17 10:00:05 +0000

    deps: add gRPC as dependency

--- images/dev/install.sh
+++ images/dev/install.sh
@@ -11,7 +11,12 @@ cat > /etc/profile.d/ossim.sh <<EOF
 export OSSIM_BUILD=/build
 export OSSIM_OUTPUT=/output
 export OSSIM_PREFIX=/prefix
+
 export PATH=\$OSSIM_PREFIX/bin\${PATH:+:\$PATH}
+export LD_LIBRARY_PATH\${OSSIM_PREFIX}/lib\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}
+export LIBRARY_PATH=\${OSSIM_PREFIX}/lib\${LIBRARY_PATH:+:\$LIBRARY_PATH}
+export CPATH=\${OSSIM_PREFIX}/include\${CPATH:+:\$CPATH}
+export PKG_CONFIG_PATH=\${OSSIM_PREFIX}/lib/pkgconfig:\${OSSIM_PREFIX}/share/pkgconfig\${PKG_CONFIG_PATH:+:\$PKG_CONFIG_PATH}
 EOF
 
 . /etc/profile.d/ossim.sh
