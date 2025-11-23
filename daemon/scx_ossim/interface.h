e6e4ecb640810a4d476e83d93e2415193a1b9430
Yiliang Wan <yiliangw@hotmail.com>
2025-11-23 05:24:33 +0000

    refactor(scx): add redundant simt in sched_node to avoid calling helpers when holding spin lock of sched_grp

--- daemon/scx_ossim/interface.h
+++ daemon/scx_ossim/interface.h
@@ -80,6 +80,7 @@ struct scx_ossim_vcpu_metadata {
   u32 vm_id;
   u32 vcpu_id;
   u64 timestamp; /* Registration timestamp */
+  u64 simt;      /* Current simulated time */
   struct scx_ossim_sync_scope sync_scope;
 };
 
