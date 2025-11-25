89ae743edcc06fbc495e51442316f60f3ff016ed
Yiliang Wan <yiliangw@hotmail.com>
2025-11-25 06:21:58 +0000

    utils: update bridge and NAT helper scripts

--- utils/rules.mk
+++ utils/rules.mk
@@ -1,5 +1,5 @@
-bridge_script := $(d)bridge.sh
-nat_script := $(d)nat.sh
+bridge_script := $(d)bridge.py
+nat_script := $(d)nat.py
 
 .PHONY: setup-bridges cleanup-bridges setup-nat cleanup-nat
 
@@ -10,41 +10,35 @@ PROVIDER_BRIDGE ?= br-ossim1
 PROVIDER_BRIDGE_CIDR := 10.10.11.1/24
 
 setup-bridges:
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(MANAGEMENT_BRIDGE) \
-	BRIDGE_IF_CIDR=$(MANAGEMENT_BRIDGE_CIDR) \
-	sudo -E bash $(bridge_script) setup
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(PROVIDER_BRIDGE) \
-	BRIDGE_IF_CIDR=$(PROVIDER_BRIDGE_CIDR) \
-	sudo -E bash $(bridge_script) setup
-
-	sudo sysctl -w net.bridge.bridge-nf-call-iptables=0
-	sudo sysctl -w net.bridge.bridge-nf-call-ip6tables=0
-	sudo sysctl -w net.bridge.bridge-nf-call-arptables=0
+	sudo python3 $(bridge_script) setup \
+		--bridge-if $(MANAGEMENT_BRIDGE) \
+		--bridge-cidr $(MANAGEMENT_BRIDGE_CIDR) \
+		--prefix $(PREFIX)
+	sudo python3 $(bridge_script) setup \
+		--bridge-if $(PROVIDER_BRIDGE) \
+		--bridge-cidr $(PROVIDER_BRIDGE_CIDR) \
+		--prefix $(PREFIX)
+	sudo modprobe br_netfilter || true
+	sudo sysctl -w net.bridge.bridge-nf-call-iptables=0 || true
+	sudo sysctl -w net.bridge.bridge-nf-call-ip6tables=0 || true
+	sudo sysctl -w net.bridge.bridge-nf-call-arptables=0 || true
 
 cleanup-bridges:
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(MANAGEMENT_BRIDGE) \
-	BRIDGE_IF_CIDR=$(MANAGEMENT_BRIDGE_CIDR) \
-	sudo -E bash $(bridge_script) cleanup 
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(PROVIDER_BRIDGE) \
-	BRIDGE_IF_CIDR=$(PROVIDER_BRIDGE_CIDR) \
-	sudo -E bash $(bridge_script) cleanup 
+	sudo python3 $(bridge_script) cleanup --bridge-if $(MANAGEMENT_BRIDGE)
+	sudo python3 $(bridge_script) cleanup --bridge-if $(PROVIDER_BRIDGE)
 
 setup-nat:
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(MANAGEMENT_BRIDGE) \
-	sudo -E bash $(nat_script) setup
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(PROVIDER_BRIDGE) \
-	sudo -E bash $(nat_script) setup
+	sudo python3 $(nat_script) setup \
+		--bridge-if $(MANAGEMENT_BRIDGE) \
+		--internet-if $(INTERNET_IF)
+	sudo python3 $(nat_script) setup \
+		--bridge-if $(PROVIDER_BRIDGE) \
+		--internet-if $(INTERNET_IF)
 
 cleanup-nat:
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(MANAGEMENT_BRIDGE) \
-	sudo -E bash $(nat_script) cleanup
-	INTERNET_IF=$(INTERNET_IF) \
-	BRIDGE_IF=$(PROVIDER_BRIDGE) \
-	sudo -E bash $(nat_script) cleanup
+	sudo python3 $(nat_script) cleanup \
+		--bridge-if $(MANAGEMENT_BRIDGE) \
+		--internet-if $(INTERNET_IF)
+	sudo python3 $(nat_script) cleanup \
+		--bridge-if $(PROVIDER_BRIDGE) \
+		--internet-if $(INTERNET_IF)
