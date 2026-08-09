36cf7bbd504ac5c813a51151242ea59664e43cce
Yiliang Wan <yiliangw@hotmail.com>
2026-08-09 23:03:46 +0800

    update ethernet ipc

--- scripts/install_apt_deps.sh
+++ scripts/install_apt_deps.sh
@@ -32,6 +32,7 @@ sudo apt-get update && sudo apt-get install -y \
     clang llvm pkg-config libelf-dev \
     protobuf-compiler libprotobuf-dev libseccomp-dev \
     libgrpc-dev libgrpc++-dev protobuf-compiler-grpc \
+    libyaml-cpp-dev \
     libglib2.0-dev libfdt-dev libpixman-1-dev \
     zlib1g-dev ninja-build \
     guestfish cloud-image-utils \
