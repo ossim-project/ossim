da77804611137316ff64e22948efe23d08427578
Yiliang Wan <yiliangw@hotmail.com>
2026-08-22 09:02:05 +0000

    add CXL shared memory support

--- make/qemu.mk
+++ make/qemu.mk
@@ -13,7 +13,8 @@ configure-qemu: install-libossim update-qemu-kernel-headers
 	cd $(qemu_b) && $(abspath $(qemu_d)configure) \
 		--prefix=$(abspath $(PREFIX)) \
 		--target-list=x86_64-softmmu \
-		--extra-cflags=-Wno-error=discarded-qualifiers \
+		--extra-cflags="-I$(PREFIX)/include" \
+		--extra-ldflags="-L$(PREFIX)/lib" \
 		--enable-numa \
 		--enable-slirp \
 		--enable-kvm \
